5141158aa9015119a11d9d46cb2713d2cf24cd58
Thierno Diallo <contact.thierno@gmail.com>
2021-10-17 13:32:00 +0000

    Add file classes.py

---
+++ classes.py
@@ -0,0 +1,30 @@
+class Point():
+    def __init__(self, f_input, s_input):
+        self.x = f_input
+        self.y = s_input
+
+p = Point(10, 30)
+
+#print(p.x)
+#print(p.y)
+
+class Stadium():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.booked_seats = []
+    def add_person(self, name):
+        if self.available_seats() == 0:
+            return False
+        self.booked_seats.append(name)
+        return True
+    def available_seats(self):
+        return self.capacity - len(self.booked_seats)
+
+stadium = Stadium(4)
+liste = ["Neymar", "Messi", "Ronaldo", "Haaland", "Mbappé"]
+
+for name in liste:
+    if stadium.add_person(name):
+        print(f"Seats availble for {name}.")
+    else:
+        print(f"No available seats for {name}.")    
